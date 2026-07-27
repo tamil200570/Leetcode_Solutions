bc0023a01799b6482723980874ee5d1c5725b4b3
Tamil Selvan <tamil.selvan.75457@gmail.com>
2026-07-27 13:35:18 +0530

    Time: 1 ms (78.73%), Space: 42.5 MB (92.43%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.java
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.java
@@ -1,39 +1,24 @@
-class Solution 
-{
-    public boolean checkZeroOnes(String s) 
-    {
-        int max1=0;
-        int max2=0;
-        int c1=0;
-        int c2=0;
-        for(int i=0;i<s.length();i++)
-        {
-            char c=s.charAt(i);
-            if(c=='1')
-            {
-                c1++;
-            }
-            else 
-            {
-                max1=Math.max(max1,c1);
-                c1=0;
-            }
-            if(c=='0')
-            {
-                c2++;
-            }
-            else 
-            {
-                max2=Math.max(max2,c2);
-                c2=0;
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        int currOne = 0;
+        int currZero = 0;
+        int maxOne = 0;
+        int maxZero = 0;
+        for(char c : s.toCharArray()){
+            if(c == '1'){
+                currOne++;
+                currZero = 0;
+                maxOne = Math.max(maxOne, currOne);
+            }else{
+                currZero++;
+                currOne = 0;
+                maxZero = Math.max(maxZero, currZero);
             }
         }
-        max1=Math.max(max1,c1);
-        max2=Math.max(max2,c2);
-        if(max1>max2)
-        {
+        if(maxOne > maxZero){
             return true;
+        }else{
+            return false;
         }
-        return false;
     }
 }
