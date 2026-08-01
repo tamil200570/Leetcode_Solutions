f219f7f262e9d1f5b3e9fdb4f6aafd2e4cc81018
Tamil Selvan <tamil.selvan.75457@gmail.com>
2026-08-01 14:43:09 +0530

    Time: 56 ms (20.81%), Space: 116.2 MB (82.51%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -5,13 +5,11 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = n - 1; i >= 0; i--) {
-            // Count all shorter people
             while (!stack.isEmpty() && stack.peek() < heights[i]) {
                 ans[i]++;
                 stack.pop();
             }
 
-            // Count the first taller or equal person
             if (!stack.isEmpty()) {
                 ans[i]++;
             }
