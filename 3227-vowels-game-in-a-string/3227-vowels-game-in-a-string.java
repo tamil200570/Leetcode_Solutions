6079dc9e5f9aa7e26ca208db3f12384cfedae290
Tamil Selvan <tamil.selvan.75457@gmail.com>
2026-08-02 10:14:10 +0530

    Time: 4 ms (69.18%), Space: 48 MB (30.62%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -1,22 +1,13 @@
 class Solution {
     public boolean doesAliceWin(String s) 
     {
-       int c=0;
        for(char ch :s.toCharArray())
        {
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
         {
-            c++;
+            return true;
         }
        }    
-       if(c==0)
-       {
-        return false;
-       }
-       else if(c%2!=0)
-       {
-        return true;
-       }
-       return true;
+      return false;
     }
 }
