d6f71905a5137a14e78894c0d9d39832e584ec29
Tamil Selvan <tamil.selvan.75457@gmail.com>
2026-08-02 10:04:31 +0530

    Time: 3 ms (86.07%), Space: 43.1 MB (73.63%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public boolean isValid(String s) 
+    {
+         Stack<Character>st=new Stack<>();
+         for(int i=0;i<s.length();i++)
+         {
+            char c=s.charAt(i);
+            if(c=='('||c=='['||c=='{')
+            {
+                st.push(c);
+            }
+            else 
+            {
+                if(st.isEmpty())
+                {
+                    return false;
+                }
+                char top =st.pop();
+                if(c==')'&&top!='('||top!='['&&c==']'||top!='{'&&c=='}')
+                {
+                    return false;
+                }
+
+            }
+         }
+         return st.isEmpty();
+
+    }
+}
