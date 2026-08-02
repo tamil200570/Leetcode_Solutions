c40cbf4ac06ce111fddcff399688a325117636f6
Tamil Selvan <tamil.selvan.75457@gmail.com>
2026-08-02 10:30:29 +0530

    Time: 2 ms (84.62%), Space: 44.5 MB (46.11%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+
+        while (s.indexOf(part) != -1) {
+            int index = s.indexOf(part);
+
+            s = s.substring(0, index) +
+                s.substring(index + part.length());
+        }
+
+        return s;
+    }
+}
