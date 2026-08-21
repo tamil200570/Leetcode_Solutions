e136b47f0df79c69d8149da28a34dea07db3e8d2
Tamil Selvan <tamil.selvan.75457@gmail.com>
2026-08-21 16:23:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (72.92%) - LeetHub

---
+++ 0887-super-egg-drop/0887-super-egg-drop.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public int superEggDrop(int k, int n) 
+    {
+        int dp[]=new int[k+1];
+        int moves =0;
+        while(dp[k]<n)
+        {
+            moves++;
+        for(int i=k;i>=1;i--)
+        {
+           dp[i]=dp[i]+dp[i-1]+1;
+        }
+        }
+        return moves;
+    }
+}
